7f415859ed87f6067d5ef2a07f2787df966289d4
Brock <0xbrock@users.noreply.github.com>
2016-10-30 14:14:22 -0400

    Add cross-platform input.  Update headcheck on Enemies for more accurate kills.

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement; // include so we can load new scenes
+using UnityStandardAssets.CrossPlatformInput;
 
 public class CharacterController2D : MonoBehaviour {
 
@@ -89,7 +90,7 @@ public class CharacterController2D : MonoBehaviour {
 			return;
 
 		// determine horizontal velocity change based on the horizontal input
-		_vx = Input.GetAxisRaw ("Horizontal");
+		_vx = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 
 		// Determine if running based on the horizontal movement
 		if (_vx != 0) 
@@ -119,10 +120,10 @@ public class CharacterController2D : MonoBehaviour {
 		// Set the grounded animation states
 		_animator.SetBool("Grounded", _isGrounded);
 
-		if(_isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
+		if(_isGrounded && CrossPlatformInputManager.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
         {
             DoJump();
-        } else if (_canDoubleJump && Input.GetButtonDown("Jump")) // If candoublejump && jump button pressed, then allow player to double jump
+        } else if (_canDoubleJump && CrossPlatformInputManager.GetButtonDown("Jump")) // If candoublejump && jump button pressed, then allow player to double jump
         {
             DoJump();
             // Disable double jump after double jumping since you can only do it once
@@ -131,7 +132,7 @@ public class CharacterController2D : MonoBehaviour {
 
         // If the player stops jumping mid jump and player is not yet falling
         // then set the vertical velocity to 0 (he will start to fall from gravity)
-        if (Input.GetButtonUp("Jump") && _vy>0f)
+        if (CrossPlatformInputManager.GetButtonUp("Jump") && _vy>0f)
 		{
 			_vy = 0f;
 		}
